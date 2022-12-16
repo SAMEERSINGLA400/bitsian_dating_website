a7cfd269d7ec212372afac69145363cc9d85c8b6
Sameer Singla <sameersingla400@gmail.com>
2022-12-16 23:13:29 +0530

    third commit

--- users/views.py
+++ users/views.py
@@ -1,11 +1,12 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
+from .forms import UserRegisterForm
 
 # Create your views here.
 def register(request):
     if request.method == 'POST':
-        form = UserCreationForm(request.POST)
+        form =UserRegisterForm(request.POST)
         if form.is_valid():
             form.save()
             username = form.cleaned_data.get('username')
@@ -13,5 +14,5 @@ def register(request):
             return redirect('meet-home')
 
     else:
-        form = UserCreationForm()
+        form = UserRegisterForm()
     return render(request, 'users/register.html', {'form': form})
