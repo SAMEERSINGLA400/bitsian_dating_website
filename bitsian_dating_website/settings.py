4d302c12e97eef28f5f05b997f8aeabce479e4ab
SAMEERSINGLA400 <120049817+SAMEERSINGLA400@users.noreply.github.com>
2023-01-13 04:03:36 +0530

    Update settings.py

--- bitsian_dating_website/settings.py
+++ bitsian_dating_website/settings.py
@@ -154,5 +154,5 @@ LOGIN_URL = 'login'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'd91771639@gmail.com'
-EMAIL_HOST_PASSWORD = 'query1234'
+EMAIL_HOST_PASSWORD = '#######'
 EMAIL_USE_TLS = True
